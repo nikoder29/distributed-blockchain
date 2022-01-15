111d7deea2ddb3eae1f82a46b39890e2142259d3
nikoder29 <nikunjbaid@ucsb.edu>
2022-01-15 11:40:18 -0800

    added logic for the blockchain server : balance_transaction and transfer_transaction

--- client/client.py
+++ client/client.py
@@ -1,5 +1,6 @@
 import socket
 import logging
+import json
 
 logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
     datefmt='%Y-%m-%d:%H:%M:%S',
@@ -12,14 +13,34 @@ HOST = '127.0.0.1'
 # The port used by the server
 PORT = 65432
 
+def display_menu():
+    print("a. Press 1 to make a new transaction.")
+    print("b. Press 2 to get balance")
+    print("c. Press 3 to quit")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     client_socket.connect((HOST, PORT))
-    client_socket.sendall(b'Hello, world')
-    data = client_socket.recv(1024)
-    logger.info('Received : ' + repr(data))
+    # client_socket.sendall(b'Hello, world')
+    # data = client_socket.recv(1024)
+    # logger.info('Received : ' + repr(data))
     while True:
+        display_menu()
         user_input = input("client prompt >> ")
-        client_socket.sendall(str.encode(user_input))
+        if user_input == "1":
+            receiver_addr = input("Enter receiver client's address  >> ")
+            #TODO : add check if receiver is available in the config list or not
+            amount = input("Enter the amount in $$ to be transferred to the above client  >> ")
+            msg_dict = {'type': 'transfer_transaction', 'receiver': receiver_addr, 'amount': amount}
+
+        elif user_input == "2":
+            msg_dict = {'type': 'balance_transaction'}
+        elif user_input == "3":
+            msg_dict = {'type': 'quit'}
+        else:
+            print("Incorrect menu option. Please try again..")
+            continue
+        msg_str = json.dumps(msg_dict)
+        client_socket.sendall(msg_str.encode())
         data = client_socket.recv(1024).decode()
         logger.info('Received : ' + repr(data))
         if data == 'quit':
---
+++ server/blockchain.py
@@ -0,0 +1,46 @@
+from typing import List
+import hashlib
+from block import Block
+
+
+class Blockchain:
+    INITIAL_CLIENT_BALANCE = 10
+
+    def __init__(self):
+        self.chain: List[Block] = []
+        self.add_block(sender='dummy_sender', receiver='dummy_receiver', amount=0, previous_hash='0')
+
+    def add_block(self, sender, receiver, amount, previous_hash):
+        new_block = Block(sender, receiver, amount, previous_hash)
+        self.chain.append(new_block)
+
+    def get_balance(self, client: str):
+        client_balance = Blockchain.INITIAL_CLIENT_BALANCE
+        for block in self.chain:
+            if block.transaction.sender == client:
+                client_balance -= block.transaction.amount
+            elif block.transaction.receiver == client:
+                client_balance += block.transaction.amount
+
+        return client_balance
+
+    def execute_transaction(self, sender, receiver, amount):
+        # check if sender has sufficient balance
+        # add block to the chain if the above holds true
+        # return True if block added successfully
+        # else return false
+        # if sender == receiver, return True without executing anything
+        sender_balance = self.get_balance(sender)
+        if sender_balance < amount:
+            return False
+
+        self.add_block(sender, receiver, amount, self.get_previous_block_hash())
+        return True
+
+    @staticmethod
+    def hash(block):
+        # encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(repr(block).encode()).hexdigest()
+
+    def get_previous_block_hash(self):
+        return Blockchain.hash(self.chain[-1])
