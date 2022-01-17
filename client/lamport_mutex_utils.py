130969fe99c6ef015b2f6489bec92d263d6a248c
Nawel Alioua <nawel@ucsb.edu>
2022-01-17 07:40:02 -0800

    Added a new module for lamport mutual exclusion utilities.

--- client/lamport_mutex_utils.py
+++ client/lamport_mutex_utils.py
@@ -1,4 +1,18 @@
+from functools import total_ordering
+
+@total_ordering
 class Timestamp:
     def __init__(self, lamport_clock, pid):
         self.lamport_clock = lamport_clock
         self.pid = pid
+    
+    def __lt__(self, obj):
+        return ((self.lamport_clock < obj.lamport_clock)\
+            or (((self.lamport_clock == obj.lamport_clock) and (self.pid < obj.pid))))
+
+    def __eq__(self, obj):
+        return ((self.lamport_clock == obj.lamport_clock)\
+            and (self.pid == obj.pid))
+  
+    def __repr__(self):
+        return " ".join(["Lamport Clock:", str(self.lamport_clock), " pid:", str(self.pid)])
